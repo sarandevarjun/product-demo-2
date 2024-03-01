3746bdd051d140d28a14c86a098a1a75008e29a6
Saravanan <saransri13@gmail.com>
2024-03-01 19:51:29 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Product demo 2`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
