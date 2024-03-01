059b4f9a73d033feae9234c21bf6a11d3fcdc7d4
Saravanan <saransri13@gmail.com>
2024-03-01 21:19:17 +0530

    Updates

---
+++ component---src-pages-index-js-8e7906d2ee01469a07a3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkproduct_demo_2=self.webpackChunkproduct_demo_2||[]).push([[678],{6558:function(e,t,n){n.r(t),n.d(t,{Head:function(){return u}});var l=n(7294);t.default=()=>l.createElement(l.Fragment,null,l.createElement("h1",null,"Gatsby Site Title!!!"));const u=()=>l.createElement("title",null,"Home Page")}}]);
+//# sourceMappingURL=component---src-pages-index-js-8e7906d2ee01469a07a3.js.map
